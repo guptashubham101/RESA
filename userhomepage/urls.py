cec264c7b535af9b4e465a10e6e01d3155f68059
laishwaryat <aishwarya.thela@gmail.com>
2021-04-21 19:12:34 -0400

    urls changed

--- userhomepage/urls.py
+++ userhomepage/urls.py
@@ -5,9 +5,8 @@ urlpatterns =[
 
 path('userhomepage/', views.userhomepage, name  ='userhomepage'),
 path('write_recipe/', WriteRecipeApiView.as_view(), name  ='writeRecipe'),
-path('', WriteRecipeApiVpath('get_Recipe/', GetRecipeView.as_view(), name='getRecipe'),
-    path('', GetRecipeView.as_view(), name  ='updateRecipe'),iew.as_view(), name  ='postRecipe'),
-#path('get_recipe/', get_recipe, name='getRecipe'),
+path('', WriteRecipeApiView.as_view(), name  ='postRecipe'),
+#ath('get_recipe/', get_recipe, name='getRecipe'),
 path('get_Recipe/', GetRecipeView.as_view(), name='getRecipe'),
 path('', GetRecipeView.as_view(), name  ='updateRecipe'),
 
